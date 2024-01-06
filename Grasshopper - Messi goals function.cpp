d3c880c3e780102967c5d5cd30530d4fed413d88
Маршалл <44672093+LunaTMT@users.noreply.github.com>
2024-01-06 12:10:00 +0000

    Add files via upload

---
+++ Grasshopper - Messi goals function.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+
+#include <vector>
+
+int goals (int a, int b, int c) {
+  return a + b + c;
+}
+
+int main() {
+    std::cout << "Hi";
+}
+
---
+++ get character from ASCII Value.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+
+#include <vector>
+
+char get_char(int i) {
+ return char(i);
+}   
+
+
+int main() {
+    std::cout << "Hi";
+}
+
