ea12a0edd56c59570ad9f7a30f8f633bd9ff3ed2
LunaTMT <taylorthreader@gmail.com>
2024-02-05 21:39:47 +0000

    adding vowel count

--- Find the Integral.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <string>
-std::string integrate(const int& a,const int& b){
-  return std::to_string(a/(b+1)) + "x^" + std::to_string(b+1);
-}
-
-int main() {
-    return 0;  // Indicates successful execution
-}
