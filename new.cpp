174e5ef8174725cadd73dd344436c3970831d15f
Маршалл <44672093+LunaTMT@users.noreply.github.com>
2024-02-14 23:01:39 +0000

    Add files via upload

--- new.cpp
+++ new.cpp
@@ -1,22 +1,6 @@
+#include <algorithm>
 #include <string>
-#include <unordered_set>
 
-using namespace std;
-
-int getCount(const string& str){
-  int result = 0;
-  
-  unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
-  for (char C : str){
-    char c = tolower(C);
-    if (vowels.count(c) > 0){
-      result ++;
-    } 
-  }
-  
-  return result;
-}
-
-int main() {
-    return 0;
+std::string reverseString(const std::string& str) {
+  return std::string(str.rbegin(), str.rend());
 }
